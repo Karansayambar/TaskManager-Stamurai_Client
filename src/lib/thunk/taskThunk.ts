0c390e299076d380c2b368bccfbd68f9ab600834
SayambarKaran <karan.subhashsayambar@quadbtech.com>
2025-05-08 13:19:30 +0530

    readme file added

--- src/lib/thunk/taskThunk.ts
+++ src/lib/thunk/taskThunk.ts
@@ -276,10 +276,8 @@ export const readAssignTasks = createAsyncThunk(
   async (_, { rejectWithValue }) => {
     try {
       const token = localStorage.getItem("tm-token");
-      const url = process.env.NEXT_PUBLIC_API_URL; // Store in variable first
-      console.log("API URL:", url); // Debug log
       const response = await fetch(
-        `${url}/todo/readAssignTask`, // Use the variable
+        `${process.env.NEXT_PUBLIC_API_URL}/todo/readAssignTask`,
         {
           method: "GET",
           headers: {
